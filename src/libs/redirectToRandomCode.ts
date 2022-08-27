8ffa07f3c224104d68beea390cf496725dfe1ebc
Daniel Fiore <taptapdan@gmail.com>
2022-08-27 13:31:43 -0500

    fix: random numbers not random
    Random number was being generated on instance of returned function, so generating a new random code would result in the same number part each time.

--- src/libs/redirectToRandomCode.ts
+++ src/libs/redirectToRandomCode.ts
@@ -7,18 +7,21 @@ import {
   uniqueNamesGenerator,
 } from 'unique-names-generator';
 
-const numberDictionary = NumberDictionary.generate({ min: 100, max: 999 });
-
-const uniqueNamesConfig: Config = {
-  dictionaries: [adjectives, animals, numberDictionary],
-  separator: '-',
-  length: 3,
-};
-
 export const redirectToRandomCode = () => {
   const router = useRouter();
 
   const linkToRandomCode = () => {
+    const numberDictionary = NumberDictionary.generate({
+      min: 100,
+      max: 999,
+    });
+
+    const uniqueNamesConfig: Config = {
+      dictionaries: [adjectives, animals, numberDictionary],
+      separator: '-',
+      length: 3,
+    };
+
     const code: string = uniqueNamesGenerator(uniqueNamesConfig);
     router.replace(`/observe/${code}`);
   };
