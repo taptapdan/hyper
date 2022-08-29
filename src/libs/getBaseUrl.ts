b10f2cbf92f5662fb3b06ab7b78ba72e2fe85526
Daniel Fiore <taptapdan@gmail.com>
2022-08-29 09:27:46 -0500

    fix: request url adapts to environment

--- src/libs/getBaseUrl.ts
+++ src/libs/getBaseUrl.ts
@@ -1,8 +1,4 @@
 export function getBaseUrl() {
-  // Browser should use current path.
-  if (typeof window !== 'undefined')
-    return `http://localhost:${process.env.PORT ?? 3000}`;
-
   // SSR should use Vercel URL.
   const VERCEL_URL =
     process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
