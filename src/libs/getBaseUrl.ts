d814c5bd1cf722cad9fee331d746b8b16c49f976
Daniel Fiore <taptapdan@gmail.com>
2022-08-29 13:20:50 -0500

    feat: use custom domain

--- src/libs/getBaseUrl.ts
+++ src/libs/getBaseUrl.ts
@@ -1,4 +1,9 @@
 export function getBaseUrl() {
+  // Use custom domain if specified and in production.
+  const CUSTOM_DOMAIN =
+    process.env.VERCEL_ENV === 'production' && process.env.NEXT_PUBLIC_BASE_URL;
+  if (CUSTOM_DOMAIN) return `https://${CUSTOM_DOMAIN}`;
+
   // SSR should use Vercel URL.
   const VERCEL_URL =
     process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
