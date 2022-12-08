af985806597ab3eda1c42a8b11cf3dd66b9974d5
Daniel Fiore <taptapdan@gmail.com>
2022-12-07 20:17:50 -0600

    refactor: getBaseUrl

--- src/libs/getBaseUrl.ts
+++ src/libs/getBaseUrl.ts
@@ -1,13 +1,16 @@
 export function getBaseUrl() {
   // Use custom domain if specified.
   const CUSTOM_DOMAIN = process.env.NEXT_PUBLIC_CUSTOM_DOMAIN;
-  if (CUSTOM_DOMAIN) return `https://${CUSTOM_DOMAIN}`;
-
   // SSR should use Vercel URL.
   const VERCEL_URL =
     process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
-  if (VERCEL_URL) return `https://${VERCEL_URL}`;
 
-  // Dev SSR should use localhost.
-  return `http://localhost:${process.env.PORT ?? 3000}`;
+  switch (true) {
+    case !!CUSTOM_DOMAIN:
+      return `https://${CUSTOM_DOMAIN}`;
+    case !!VERCEL_URL:
+      return `https://${VERCEL_URL}`;
+    default:
+      return `http://localhost:${process.env.PORT || 3000}`;
+  }
 }
